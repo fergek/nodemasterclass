beeb6147775915c7490fc75c9fd6ab5ac709661c
Fernando <fgekdyszman@makingsense.com>
2021-10-28 11:05:10 -0300

    logging url

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ Primary file for API
 
 //Dependencies
 var http = require('http');
+var url = require ('url');
 
 // Server should respond to all with a string
 var server = http.createServer(function (req, res) {
-    res.end("HEllo world\n");
+    // Get URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the PATH for URL
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+
+    // send response
+    res.end('Hello World!');
+
+    // log the path the person is asking for
+    console.log('Request received on ' + trimmedPath);
+
 })
 
 // Start the server on port 3000
